34a8819e6f69b57adcc8e33eb2e5a32a98b3099c
tanveer <tanveeratworks@gmail.com>
2024-06-12 09:45:19 +0530

    Setup Dagger-Hilt lib

--- app/src/main/java/com/example/todo_jetpack/data/ToDoDatabase.kt
+++ app/src/main/java/com/example/todo_jetpack/data/ToDoDatabase.kt
@@ -1,10 +1,11 @@
 package com.example.todo_jetpack.data
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.todo_jetpack.data.models.ToDoTask
 
 @Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
-abstract class ToDoDatabase {
+abstract class ToDoDatabase: RoomDatabase() {
 
     abstract fun toDoDao(): ToDoDao
 }
